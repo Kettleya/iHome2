d0b96caf462ddf1ac85da052a2b84a5f21152dea
Kettleya <kekechuia@gmail.com>
2018-02-05 21:27:03 +0800

    保存订单

--- iHome/api_1_0/order.py
+++ iHome/api_1_0/order.py
@@ -1,10 +1,8 @@
 # -*- coding:utf-8 -*-
 import datetime
-
 from flask import current_app
 from flask import g
 from flask import request, jsonify
-
 from iHome import db
 from iHome.api_1_0 import api
 from iHome.models import House, Order
@@ -12,58 +10,64 @@ from iHome.utils.common import login_required
 from iHome.utils.response_code import RET
 
 
-@api.route('/orders', methods=['POST'])
+@api.route("/orders", methods=["POST"])
 @login_required
-def make_orders():
+def create_order():
     """
-    1.获取参数并校验,住房id,用户id,开始日期,结束日期
-    2.从数据库中查询房屋状态(是否已被预定)
-    3.如果未被预定,则创建订单及相关数据
-    4.将数据保存到数据库
-    5.给出响应
+    添加新订单
+    1. 获取参数：房屋id，开始入住时间，结束入住时间
+    2. 判断参数/校验参数
+    3. 判断当前房屋在当前时间段内是否已经被预订
+    4. 创建订单模型并设置相关数据
+    5. 添加到数据库
+    6. 返回响应
     :return:
     """
-    # 1.获取参数并校验
-    json_dict = request.json
-    start_date_str = json_dict.get('start_date')
-    end_date_str = json_dict.get('end_date')
-    house_id = json_dict.get('house_id')
 
-    if not all([start_date_str, end_date_str, house_id]):
-        return jsonify(errno=RET.PARAMERR, errmsg='参数有误')
+    # 1. 获取参数：房屋id，开始入住时间，结束入住时间
+    data_dict = request.json
+    house_id = data_dict.get("house_id")
+    start_date_str = data_dict.get("start_date")
+    end_date_str = data_dict.get("end_date")
 
-    # 判断时间参数
+    # 2. 判断参数/校验参数
+
+    if not all([house_id, start_date_str, end_date_str]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 判断参数
     try:
-        # 转为时间参数
-        start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d')
-        end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d')
+        # 转成时间对象
+        start_date = datetime.datetime.strptime(start_date_str, "%Y-%m-%d")
+        end_date = datetime.datetime.strptime(end_date_str, "%Y-%m-%d")
         if start_date and end_date:
-            assert start_date < end_date, Exception('开始时间必须小于结束时间')
+            assert start_date < end_date, Exception("结束日期必须大于开始时间")
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.PARAMERR, errmsg='参数有误')
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
 
-    # 查询房屋是否存在
+    # 判断房屋是否存在
     try:
         house = House.query.get(house_id)
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR, errmsg='查询数据错误')
+        return jsonify(errno=RET.DBERR, errmsg="查询数据失败")
 
     if not house:
-        return jsonify(errno=RET.NODATA, errmsg='该房屋不存在')
+        return jsonify(errno=RET.NODATA, errmsg="未查询到房屋数据")
 
-    # 2.查询当前房屋状态,是否已被预定
+    # 3. 判断当前房屋在当前时间段内是否已经被预订
     try:
-        conflict_order = Order.query.filter(end_date>Order.begin_date,start_date<Order.end_date,house_id==House.id).all()
+        conflict_orders = Order.query.filter(end_date > Order.begin_date, start_date < Order.end_date,
+                                             Order.house_id == house_id).all()
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR, errmsg='查询数据错误')
+        return jsonify(errno=RET.DBERR, errmsg="查询数据错误")
 
-    if conflict_order:
-        return jsonify(errno=RET.DATAERR, errmsg='该房间已被预定')
+    if conflict_orders:
+        return jsonify(errno=RET.DATAERR, errmsg="当前房屋已被预订")
 
-    # 3.创建订单模型,存储数据
+    # 4. 创建订单模型并设置相关数据
     days = (end_date - start_date).days
     order = Order()
     order.user_id = g.user_id
@@ -71,19 +75,19 @@ def make_orders():
     order.begin_date = start_date
     order.end_date = end_date
     order.days = days
-    order.price= house.price
-    order.amount = days*house.price
+    order.house_price = house.price
+    order.amount = days * house.price
 
-    # 设置房屋订单数量+1
+    # 设置房屋的订单数量加1
     house.order_count += 1
 
-    # 4.保存数据
+    # 5. 添加到数据库
     try:
         db.session.add(order)
         db.session.commit()
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR, errmsg='保存订单失败')
+        return jsonify(errno=RET.DBERR, errmsg="保存订单失败")
 
-    # ５．给出响应
-    return jsonify(errno=RET.OK, errmsg='OK')
+    # 6. 返回响应
+    return jsonify(errno=RET.OK, errmsg="OK")
